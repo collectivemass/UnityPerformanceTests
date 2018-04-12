d240e69124143d67d0b0bc42e3dff28bd8b667dc
Roger Miller <roger@collectivemass.com>
2018-04-12 13:56:28 -0700

    Local variable assignment in a loop

---
+++ Assets/Scripts/localVariableAssignment.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class localVariableAssignment : MonoBehaviour {
+
+    public Transform assetToRotate;
+
+    private void Update () {
+        RotateWithLocalAssignment();
+    }
+
+    private void RotateWithLocalAssignment(){
+        Vector3 eularRotation = assetToRotate.localEulerAngles;
+        eularRotation.y += Time.smoothDeltaTime * 10f;
+        assetToRotate.localEulerAngles = eularRotation;
+    }
+}
---
+++ Assets/Scripts/localVariableAssignmentSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class localVariableAssignmentSpawner : MonoBehaviour {
+
+    public GameObject assetToSpawn;
+    public int totalInstances = 10000;
+    public float radius = 20;
+
+    private void Awake () {
+
+        for(int i=0; i<totalInstances; i++){
+            Transform instance = Instantiate<GameObject>(assetToSpawn).transform;
+            instance.parent = transform;
+            instance.localPosition = Random.onUnitSphere * radius;
+        }
+    }
+}
